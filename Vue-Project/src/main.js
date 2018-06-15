9f2ad69225bf48566617cac5cea0661229ec4497
legang.li@cloudscreen.com <legang.li@cloudscreen.com>
2018-06-15 11:33:51 +0800

    解决ie浏览器打不开问题

--- Vue-Project/src/main.js
+++ Vue-Project/src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
+import babelpolyfill from 'babel-polyfill'//解决ie浏览器不能加载问题
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import "../static/iconfont/iconfont.css"
